147b7825af73480c2bcbe3a6c51f5241f3b94e3a
chinhle <chinhle@chinhs-mbp.attlocal.net>
2019-08-03 06:16:39 -0400

    Finish layout and routes

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Spreedly Airlines Sample App"
+  end
+
+  test "should get new" do
+    get signup_path
+    assert_response :success
+    assert_select "title", "Sign up | #{@base_title}"
+  end
+end
